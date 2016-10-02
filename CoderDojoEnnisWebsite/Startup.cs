595d95d7c425f07bd094ebb06898dcaad46bdf37
Russ Painter <russp@geekymonkey.com>
2016-10-02 02:15:43 +0100

    Copied into release .net core empty template project

---
+++ CoderDojoEnnisWebsite/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CoderDojoEnnisWebsite
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            /*
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+            */
+
+            var fileServerOptions = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                EnableDirectoryBrowsing = false
+            };
+            //FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)fileServerOptions.StaticFileOptions.ContentTypeProvider;
+            //contentTypeProvider.Mappings.Remove(".svg");
+            //contentTypeProvider.Mappings.Add(".svg", "image/svg+xml");
+            app.UseFileServer(fileServerOptions);
+
+            //app.UseErrorPage();
+
+            // ToDo: Only route to this from /index.php/calendar/
+            app.UseStatusCodePagesWithRedirects("/#/WhenWhereModal");
+
+        }
+    }
+}
