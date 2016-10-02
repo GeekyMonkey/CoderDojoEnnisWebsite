9d9c6307c54d00a3c4860b0b3cd5c6da919169fe
Russ Painter <russp@geekymonkey.com>
2016-10-02 01:40:32 +0100

    remove bower

--- src/WebApplication3/gulpfile.js
+++ src/WebApplication3/gulpfile.js
@@ -6,16 +6,7 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var bower = require('gulp-bower');
 
-gulp.task('bower', function () {
-    // Copy angular
-    //gulp.src([
-    //'node_modules/systemjs/dist/*.*',
-    //]).pipe(gulp.dest('wwwroot/lib/systemjs'));
-
-    //return bower().pipe(gulp.dest('wwwroot/lib/'))
-});
 
 gulp.task('tsbuild', function () {
     var tsResult = gulp.src('ts/*.ts')
@@ -28,4 +19,4 @@ gulp.task('tsbuild', function () {
     .pipe(gulp.dest("wwwroot/js"));
 });
 
-gulp.task('default', ['bower', 'tsbuild']);
+gulp.task('default', ['tsbuild']);
