baf2b446ebfbdc3a4e3b459c9126f5ad084da32d
Russ Painter <russp@geekymonkey.com>
2018-04-12 23:15:39 +0100

    Update to NetCore 2.0 and VS2017

---
+++ CoderDojoEnnisWebsite/Controllers/WebPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderDojoEnnisWebsite.Controllers
+{
+    public class WebPageController : Controller
+    {
+        [Route("foo")]
+        public IActionResult Index()
+        {
+            return Content("boop");
+        }
+    }
+}
--- CoderDojoEnnisWebsite/Startup.cs
+++ CoderDojoEnnisWebsite/Startup.cs
@@ -1,56 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.StaticFiles;
 
 namespace CoderDojoEnnisWebsite
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
+            services.AddResponseCaching();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            /*
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-            */
+            var rewriteOptions = new Microsoft.AspNetCore.Rewrite.RewriteOptions().AddRedirect("index.php/calendar.*", "/#/WhenWhereModal");
+            app.UseRewriter(rewriteOptions);
 
-            var fileServerOptions = new FileServerOptions
-            {
-                EnableDefaultFiles = true,
-                EnableDirectoryBrowsing = false
-            };
-            //FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)fileServerOptions.StaticFileOptions.ContentTypeProvider;
-            //contentTypeProvider.Mappings.Remove(".svg");
-            //contentTypeProvider.Mappings.Add(".svg", "image/svg+xml");
-            app.UseFileServer(fileServerOptions);
+            app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = { "index.html" } });
 
-            //app.UseErrorPage();
+            app.UseStaticFiles();
 
-            // ToDo: Only route to this from /index.php/calendar/
-            app.UseStatusCodePagesWithRedirects("/#/WhenWhereModal");
+            app.UseMvc();
 
+            app.UseResponseCaching();
         }
     }
 }
