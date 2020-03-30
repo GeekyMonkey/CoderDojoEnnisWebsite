50610343265c43b5c5c1cc81a33a0cb33bdf9dd7
Russ Painter <russp@geekymonkey.com>
2020-03-30 22:47:13 +0100

    verification api

--- CoderDojoEnnisWebsite/Controllers/WebPageController.cs
+++ CoderDojoEnnisWebsite/Controllers/WebPageController.cs
@@ -13,5 +13,16 @@ namespace CoderDojoEnnisWebsite.Controllers
         {
             return Content("boop");
         }
+
+        [Route(".well-known/brave-rewards-verification.txt")]
+        public IActionResult BraveRewards()
+        {
+            return Content(@"This is a Brave Rewards publisher verification file.
+
+Domain: coderdojoennis.com
+Token: 51d24e9455ed94d33d2c6a4ec3fd146b95978b90bef2cd8c2d4e223545eb692c
+", 
+            "text/plain" );
+        }
     }
 }
