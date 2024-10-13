215fa38b7011d8ff97ff31c885bec5cabd440c6c
Russ Painter <russp@geekymonkey.com>
2024-10-13 12:59:52 +0100

    Create a route for the root index and redirect to the static home url

---
+++ src/app/pages/(public)/index.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script lang="ts" setup>
+/** Temporarily redirect to the old static SPA page 😝 */
+window.location.href = "/home.html";
+</script>
+
+<style scoped lang="scss"></style>
