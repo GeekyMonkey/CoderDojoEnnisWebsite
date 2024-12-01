d30709c5b30b1c40d183b84b156c83230bcd53f4
Russ Painter <russp@geekymonkey.com>
2024-12-01 22:22:20 +0000

    cloudflare build attempt

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,6 +2,11 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	alias: {
+		"process/": "process",
+		// string_decoder: "string_decoder/",
+	},
+
 	app: {
 		head: {
 			title: "CoderDojo Ennis",
