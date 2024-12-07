1682335be5b6f31567061a697211fe55e4c16f50
Russ Painter <russp@geekymonkey.com>
2024-12-07 23:18:31 +0000

    Standardize hash

--- src/server/utils/authUtils.test.ts
+++ src/server/utils/authUtils.test.ts
@@ -6,34 +6,33 @@ describe("GeneratePasswordHash", () => {
 	const pass1Upper = "PASSword-123";
 	const salt1 = "_Salty_";
 	const salt2 = "!!Umami!!";
-	const isCloudflare = false;
 
 	it("should return null if password is empty", async () => {
-		expect(await GeneratePasswordHash("", salt1, isCloudflare)).toBeNull();
+		expect(await GeneratePasswordHash("", salt1)).toBeNull();
 	});
 
 	it("should generate a hash for a given password", async () => {
 		const password = "password123";
 		const expectedHash = "iLSac0sNdXZ6-S4VUYGi6g"; // Example hash, replace with actual expected hash
-		const hash = await GeneratePasswordHash(password, salt1, isCloudflare);
+		const hash = await GeneratePasswordHash(password, salt1);
 		expect(hash).toBe(expectedHash);
 	});
 
 	it("should generate different hashes for different passwords", async () => {
-		expect(await GeneratePasswordHash(pass1, salt1, isCloudflare)).not.toBe(
-			await GeneratePasswordHash(pass2, salt1, isCloudflare),
+		expect(await GeneratePasswordHash(pass1, salt1)).not.toBe(
+			await GeneratePasswordHash(pass2, salt1),
 		);
 	});
 
 	it("should generate same hashes for different case", async () => {
-		expect(await GeneratePasswordHash(pass1, salt1, isCloudflare)).toBe(
-			await GeneratePasswordHash(pass1Upper, salt1, isCloudflare),
+		expect(await GeneratePasswordHash(pass1, salt1)).toBe(
+			await GeneratePasswordHash(pass1Upper, salt1),
 		);
 	});
 
 	it("should generate different hashes for different salt", async () => {
-		expect(await GeneratePasswordHash(pass1, salt1, isCloudflare)).not.toBe(
-			await GeneratePasswordHash(pass1, salt2, isCloudflare),
+		expect(await GeneratePasswordHash(pass1, salt1)).not.toBe(
+			await GeneratePasswordHash(pass1, salt2),
 		);
 	});
 });
--- src/server/utils/authUtils.ts
+++ src/server/utils/authUtils.ts
@@ -1,5 +1,3 @@
-// import { createHash } from "crypto";
-// import { md5 } from "cf-workers-hash";
 import hasher from "crypto-js/sha256";
 import CryptoJS from "crypto-js";
 
@@ -9,7 +7,6 @@ import CryptoJS from "crypto-js";
 export const GeneratePasswordHash = async (
 	password: string,
 	salt: string,
-	isCloudflare?: boolean,
 ): Promise<string | null> => {
 	if (!password) {
 		return null;
@@ -18,21 +15,8 @@ export const GeneratePasswordHash = async (
 	const toHash = `${passClean}-${salt}`;
 	console.log("toHash", toHash);
 
-	// let hashBuffer: Buffer;
-	// if (isCloudflare ?? IsCloudflare()) {
-	// 	const myDigest = await md5(toHash);
-	// 	hashBuffer = Buffer.from(myDigest, "binary");
-	// } else {
-	// 	hashBuffer = createHash("md5").update(toHash).digest();
-	// }
-
 	const hash = hasher(toHash);
-	const hash64: string =
-		hash.toString(CryptoJS.enc.Base64) +
-		"__" +
-		hash.toString(CryptoJS.enc.Hex);
-
-	// const hash64 = hashBuffer.toString("base64");
+	const hash64: string = hash.toString(CryptoJS.enc.Hex);
 	// console.log("hash64", hash64);
 
 	const base64String = hash64
