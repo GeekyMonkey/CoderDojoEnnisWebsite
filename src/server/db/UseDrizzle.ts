e1c539a8c699ce5fe43c71bbb70ce1ae9be74f2f
Russ Painter <russp@geekymonkey.com>
2024-12-07 14:49:48 +0000

    Change drizzle driver

--- src/server/db/UseDrizzle.ts
+++ src/server/db/UseDrizzle.ts
@@ -1,5 +1,5 @@
 import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schemas from "./schema/schemas";
 import { Hyperdrive } from "@cloudflare/workers-types";
 
@@ -14,6 +14,32 @@ export interface env {
 	// If you set another name in wrangler.toml as the value for 'binding',
 	// replace "HYPERDRIVE" with the variable name you defined.
 	HYPERDRIVE: Hyperdrive;
+}
+
+/**
+ * Create a drizzle instance for type-safe database access
+ */
+export function UseDrizzle() {
+	const config = useRuntimeConfig();
+	const dbUrl: string = GetDrizzleConnecionString;()
+
+	const hdConfig: Hyperdrive = process.env
+		.HYPERDRIVE as unknown as Hyperdrive;
+	const hdConnectionString: string | null =
+		hdConfig?.connectionString ?? null;
+	if (typeof env !== 'undefined' && env?.HYPERDRIVE && env.HYPERDRIVE.connectionString) {
+		// Running from Cloudflare Workers
+		dbUrl = hdConnectionString;
+	} else {
+		// Running from local machine
+		dbUrl = `postgresql://${config.private.supabase.user}:${config.private.supabase.userpass}@${config.private.supabase.host}:${config.private.supabase.port}/postgres`;
+	}
+
+	const client = postgres(dbUrl, {
+		prepare: false,
+	});
+	const db = drizzle({ client, schema: { ...schemas }, logger: loggingDb });
+	return db;
 }
 
 /**
