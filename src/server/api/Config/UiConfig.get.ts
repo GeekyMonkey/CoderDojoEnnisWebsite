c98caac95512012df03e60bbe95b20aabd4560b8
Russ Painter <russp@geekymonkey.com>
2024-12-12 02:43:37 +0000

    config api - log cs

--- src/server/api/Config/UiConfig.get.ts
+++ src/server/api/Config/UiConfig.get.ts
@@ -34,9 +34,9 @@ export default defineEventHandler(
 		);
 
 		try {
-			// logs.push(
-			// 	`Drizzle connection string: ${GetDrizzleConnecionString()}`,
-			// );
+			logs.push(
+				`Drizzle connection string: ${GetDrizzleConnecionString(event)}`,
+			);
 			// logs.push(
 			// 	`HYPERDRIVE Cs: ${ServerContext?.cloudflare?.env?.NUXT_HYPERDRIVE?.connectionString}`,
 			// );
